220ca5c5862096682e884ec2bcd84399f8233e66
bluecollar-coder <anishkumarmalik@gmail.com>
2022-05-04 18:22:13 -0400

    add conditions to frontend script

---
+++ components/commonfunctions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+checkUserPermissions()
+{
+  USER_ID=$(id -u)
+  if [ "$USER_ID" -ne 0 ]; then
+    echo "You are supposed to run script as a root user"
+    exit 1
+  else
+    echo "OK"
+  fi
+}
+
+statusCheck()
+{
+  if [ $1 -eq 0];then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+    exit 1
+  if
+}
--- components/frontend.sh
+++ components/frontend.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ]; then
-  echo "You are supposed to run script as a root user"
-else
- yum install nginx -y
- systemctl enable nginx
- systemctl start nginx
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
- cd /usr/share/nginx/html
- rm -rf *
- unzip /tmp/frontend.zip
- mv frontend-main/* .
- mv static/* .
- rm -rf frontend-master README.md
- mv localhost.conf /etc/nginx/default.d/roboshop.conf
- systemctl restart nginx
- fi
+source components/commonfunctions.sh
+checkUserPermissions
+
+ECHO "Installing Nginx"
+yum install nginx -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Downloading Frontend Code"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
+statusCheck $?
+
+cd /usr/share/nginx/html
+
+ECHO "Removing Old Files"
+rm -rf * &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Extracting Zip Content"
+unzip /tmp/frontend.zip &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Copying extracted Content"
+mv frontend-main/* . &>>${LOG_FILE} && mv static/* . &>>${LOG_FILE} && rm -rf frontend-main README.md &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Copy RoboShop Nginx Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Start Nginx Service"
+systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
+statusCheck $?
